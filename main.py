35cdc37943d827208fa419666234f74f01cd70cd
Jon Bell <github@lot23.com>
2020-09-16 01:11:15 -0700

    Removing the line

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ MINUTES_BETWEEN_SCRAPES = 1
 MAX_WORKERS = 10  # Number of threads to use
 #
 
-# this is jon making sure his PR works
-
 def rss_single_feed(feed_url: str) -> List[Dict[str, str]]:
     """
     :param feed_url: Url of a rss feed
